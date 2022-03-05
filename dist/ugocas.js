89c39f38a0a5e9e5a3d9ca5c70f5350d3aec537b
kesompochy <kesompochy@gmail.com>
2022-03-05 17:07:02 +0900

    initial commit

---
+++ dist/ugocas.js
@@ -0,0 +1,29 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("UGOCAS", [], factory);
+	else if(typeof exports === 'object')
+		exports["UGOCAS"] = factory();
+	else
+		root["UGOCAS"] = factory();
+})(self, function() {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
+//# sourceMappingURL=ugocas.js.map
